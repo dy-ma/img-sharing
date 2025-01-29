cc299633fe2a4b5b14c0ba146adf2d7888df8ed6
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2025-01-28 22:37:01 -0800

    Connect prisma and define schema

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
 // Create a record for an image
 export async function POST(req: NextRequest): Promise<NextResponse> {
     return new NextResponse(null, { status: 200 })
